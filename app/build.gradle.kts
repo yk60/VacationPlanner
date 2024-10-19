99114f944e89ae5d3e7d801a9298d0fd20cd3911
yk60 <youkyoung546@gmail.com>
2024-10-18 18:10:32 -0700

    use the map API and show map

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,9 @@
 plugins {
-    alias(libs.plugins.android.application)
+     alias(libs.plugins.android.application)
+     alias(libs.plugins.google.android.libraries.mapsplatform.secrets.gradle.plugin)
+//    id("com.android.application")
+//    id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin")
+    
 }
 
 android {
@@ -29,14 +33,25 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+    buildFeatures {
+        viewBinding = true
+    }
+
+
 }
+secrets {
+    propertiesFileName = "secrets.properties"
+    defaultPropertiesFileName = "local.defaults.properties"
 
+    ignoreList.add("keyToIgnore")
+    ignoreList.add("sdk.*")
+}
 dependencies {
-
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.play.services.maps)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
